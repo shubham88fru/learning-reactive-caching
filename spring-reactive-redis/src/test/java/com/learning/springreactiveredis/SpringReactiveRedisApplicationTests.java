14cbd01455cab7635aa2ac99e09446fa44f58c85
shubham88fru <shubham88fru@yahoo.com>
2025-05-31 15:12:20 -0700

    initial commit

---
+++ spring-reactive-redis/src/test/java/com/learning/springreactiveredis/SpringReactiveRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springreactiveredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
